de95385fb00bcd1880f2d7165c3bda1578cae13c
Bruno Mioto <brunomioto97@gmail.com>
2021-12-29 19:46:17 -0300

    Add files via upload

---
+++ get_data.R
@@ -0,0 +1,31 @@
+library(reservatoriosBR)
+library(dplyr)
+library(magrittr)
+library(purrr)
+
+
+reservatorios_dif <- reservatoriosBR::tabela_reservatorios() %>% 
+  distinct(codigo) %>% 
+  head()
+
+#testecsv <- read.csv("https://raw.githubusercontent.com/brunomioto/pesquisas_eleitorais/master/database/base.csv")
+
+busca_res <- function(codigo_reservatorio){
+  
+  reservatoriosBR::reservatorio_sin(codigo_reservatorio, data_inicial = "2021-01-01", data_final = "2021-01-31")
+  
+  
+}
+
+dados_reservatorios <- purrr::map_dfr(reservatorios_dif$codigo,
+                                      busca_res)
+
+
+#new_data <- rbind(testecsv, dados_reservatorios)
+
+new_data2 <- dados_reservatorios %>% 
+  distinct() %>% 
+  arrange(codigo_reservatorio, data)
+
+# salvar a versao csv
+write.csv(new_data2, "dados/reservatorios.csv")
